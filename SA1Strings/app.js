ab2a48b67f8af1aac7f1dbba781f018227742ee3
failingforward <33079995+failingforward@users.noreply.github.com>
2019-03-14 07:11:22 +0400

    Add files via upload

--- SA1Strings/app.js
+++ SA1Strings/app.js
@@ -1,6 +1,6 @@
 const input = document.getElementsByTagName('input')[0];
 const output = document.getElementsByTagName('div')[0];
-const re = /(^[\w '.?!",]+)/gm
+const re = /(^[\w '.?!",\n]+)/gm
 
 async function init (files) {
     if (files.length != 2) return alert('Error. Min/Max 2 files.');
@@ -12,31 +12,30 @@ async function init (files) {
 }
 
 function parse (text) {
-    let result;
-    let words = [];
-    while ((result = re.exec(text[0])) !== null) {
-        words.push({
-            text: result[1],
-            index: result.index
-        })
-    }
-    let arr_end = [];
-    for (word of words) {
-        let isTrue = text[1].indexOf(word.text);
-        if (isTrue != -1) {
-            arr_end.push({
-                word: word.text,
-                pos: [word.index, isTrue]
-            });
+    let first_lines = text[0].split('\n');
+    let second_lines = text[1].split('\n');
+    let lines = [];
+    let print_lines = [];
+    for (let i = 0; i < first_lines.length; i++) {
+        for (let j = 0; j < second_lines.length; j++) {
+            if (first_lines[i] == second_lines[j])
+                lines.push({
+                    word: first_lines[i],
+                    pos: [i + 1, j + 1]
+                });
         }
     }
-    print(arr_end);
+    for (line of lines) {
+        if (line.word.indexOf('=') == -1 && line.word.length > 1)
+            print_lines.push(line);
+    }
+    print(print_lines);
 }
 
 function print (lines) {
     for (line of lines) {
         let div = document.createElement('div');
-        div.innerText = `[${line.word}] [${line.pos[0]}, ${line.pos[1]}]`;
+        div.innerText = `[${line.word.slice(0, line.word.length - 1)}] [${line.pos[0]}, ${line.pos[1]}]`;
         output.appendChild(div);
     }
 }
